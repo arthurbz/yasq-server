2cd330e46f549c4c9f57d42553b92e6673896da2
arthurbz <arthurbziero@gmail.com>
2023-05-20 22:06:42 -0300

    create getCurrentState so joining user is in the same room state as every other user

--- src/room/Room.ts
+++ src/room/Room.ts
@@ -1,18 +1,58 @@
+import { Song } from "../types/models/Song.js"
+import dayjs from "dayjs"
+
 class Room {
-    private _isPlaying: boolean
-    id: string
+    public id: string
+    private isPlaying: boolean
+    private currentSong: Song | null
+    private startedAt: number | null
+    private pausedAt: number | null
+
+    private readonly placeholderSong: Song = {
+        id: "6442e0d686696084024069e1",
+        originId: "c0-hvjV2A5Y",
+        name: "Fred again.. | Boiler Room: London",
+        source: "youtube",
+        artist: "Boiler Room",
+        thumbnail: "https://i.ytimg.com/vi/c0-hvjV2A5Y/maxresdefault.jpg"
+    }
 
     constructor(id: string) {
         this.id = id
-        this._isPlaying = false
+        this.isPlaying = false
+        this.currentSong = null
+        this.startedAt = null
+        this.pausedAt = null
     }
 
-    get isPlaying(): boolean {
-        return this._isPlaying
+    getState() {
+        return {
+            isPlaying: this.isPlaying,
+            currentSong: this.placeholderSong,
+            songElapsedTime: this.getSongElapsedTime()
+        }
     }
 
-    set isPlaying(isPlaying: boolean) {
-        this._isPlaying = isPlaying
+    getSongElapsedTime() {
+        const songElapsedTime = this.isPlaying ? dayjs().unix() : this.pausedAt
+        return songElapsedTime - this.startedAt
+    }
+
+    play() {
+        if (this.isPlaying)
+            return
+
+        this.isPlaying = true
+        const songElapsedTime = this.pausedAt - this.startedAt
+        this.startedAt = dayjs().unix() - songElapsedTime
+    }
+
+    pause() {
+        if (!this.isPlaying)
+            return
+
+        this.isPlaying = false
+        this.pausedAt = dayjs().unix()
     }
 }
 
--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "./app.js"
 import { RoomMultiton } from "./room/RoomMultiton.js"
-import { Message } from "./types/Message.js"
+import { Message } from "./types/models/Message.js"
 
 io.on("connection", socket => {
     socket.on("joinRoom", (roomId: string) => {
@@ -11,17 +11,24 @@ io.on("connection", socket => {
 
     socket.on("play", (roomId: string) => {
         const room = RoomMultiton.getInstance(roomId)
+        room.play()
         io.in(room.id).emit("play")
         console.log("Play - Room:", room.id)
     })
 
     socket.on("pause", (roomId: string) => {
         const room = RoomMultiton.getInstance(roomId)
+        room.pause()
         io.in(room.id).emit("pause")
         console.log("Pause - Room:", room.id)
     })
 
     socket.on("sendMessage", (message: Message) => {
         io.in(message.roomId).emit("receiveMessage", message)
+    })
+
+    socket.on("currentState", (roomId: string) => {
+        const room = RoomMultiton.getInstance(roomId)
+        socket.emit("currentState", room.getState())
     })
 })
